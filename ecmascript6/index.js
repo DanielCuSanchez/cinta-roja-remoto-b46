c6aecbe43a3586dd9d4c032b465f6c92dc8d1a28
daniel cu <danielcu@alternet.com.mx>
2020-10-21 21:26:41 -0500

    Funciones flecha

---
+++ ecmascript6/index.js
@@ -0,0 +1,32 @@
+
+function suma(num1, num2){
+    return num1 + num2;
+}
+
+const suma1 = (num1, num2)=>{
+    return num1 +num2;
+}
+const suma2 = (num1, num2) => num1 +num2;
+
+console.log(suma(3,90));
+
+
+const saludar = nombre => console.log(`Saludos ${nombre}`);
+
+saludar('Grupo Voltio')
+
+
+//setTimeout(()=>{console.log('object')},3000)
+// setTimeout(function(){
+//     console.log('object')
+// },3000)
+
+
+// suma(2,90)
+// console.log(suma)
+// console.log(suma1)
+
+
+
+// console.log(suma(3,90));
+// console.log(suma1(3,90));
