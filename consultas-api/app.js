dd179906d969da58cb5382b31de2326e3c16dc57
DanieÃl Cu <danielcu@alternet.com.mx>
2020-10-28 21:41:53 -0600

    axios

--- consultas-api/app.js
+++ consultas-api/app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const axios = require('axios');
 
 //console.log(request);
 
@@ -13,13 +14,30 @@ const getWeatherByCity = ( city ) => {
                 return
             }
             if(response.statusCode === 200){
-                resolve(body)
+                resolve(body);
             }
         })
     })
 }
-getWeatherByCity('London')
-.then(respuesta => {
-    console.log(respuesta);
-})
-.catch(e => console.log(e))
+// getWeatherByCity('London')
+// .then(respuesta => {
+//     console.log(respuesta);
+// })
+// .catch(e => console.log(e));
+
+
+const getWeatherByCity2 = ( city )=>{
+    return new Promise((resolve, reject)=>{
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=74835d738b5362c91b3b5ab9a489aecb `;
+        axios.get(URL)
+            .then(response =>{
+                const { data } = response;
+                resolve(data);
+            })
+            .catch(e => reject(e));
+    })
+}
+getWeatherByCity2('London')
+.then( console.log )
+//then (r => console.log(r));
+.catch(console.log )
